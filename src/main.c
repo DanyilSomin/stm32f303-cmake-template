3b7d18427825c7f70515248efc08cfb7adbed9aa
Danyil Somin <danyil.somin@gmail.com>
2021-03-08 23:00:33 +0200

    added freertos

--- src/main.c
+++ src/main.c
@@ -1,18 +1,14 @@
 #include "stm32f30x.h"
 #include "stm32f30x_gpio.h"
-#include "stm32f30x_rcc.h"
+#include "stm32f30x_rcc.h" 
 
-#define DELAY_TIME 45
+#include "FreeRTOS.h"
+#include "task.h" 
 
 GPIO_InitTypeDef gpio;
-unsigned int i;
-
-static volatile uint32_t msTicks = 0;
-void delay_ms(uint32_t dlyTicks);	
 
 void initAll()
 {
-	SysTick_Config(SystemCoreClock / 1000);
     RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOE, ENABLE);
     gpio.GPIO_Mode = GPIO_Mode_OUT;
     gpio.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_8 | GPIO_Pin_10 | GPIO_Pin_15 |
@@ -20,56 +16,55 @@ void initAll()
     GPIO_Init(GPIOE, &gpio);
 }
 
-
-void SysTick_Handler (void) //Enter here every 1 ms
-{
-  msTicks++;
-}
-
-void delay_ms(uint32_t dlyTicks)
-{
-  uint32_t curTicks;
-
-  curTicks = msTicks;
-  while ((msTicks - curTicks) < dlyTicks);
-}
-
-int main()
-{
-    initAll();
-    
-    while(1)
+void prvTaskA (void* pvParameters)
+{		
+    (void) pvParameters;
+	
+    for (;;) 
     {
         GPIO_ResetBits(GPIOE, GPIO_Pin_15);
         GPIO_SetBits(GPIOE, GPIO_Pin_8);
-        delay_ms(DELAY_TIME);
+        vTaskDelay(500);
 
 		GPIO_ResetBits(GPIOE, GPIO_Pin_8);
         GPIO_SetBits(GPIOE, GPIO_Pin_9);
-        delay_ms(DELAY_TIME);
+        vTaskDelay(500);
 
 		GPIO_ResetBits(GPIOE, GPIO_Pin_9);
         GPIO_SetBits(GPIOE, GPIO_Pin_10);
-        delay_ms(DELAY_TIME);
+        vTaskDelay(500);
 
 		GPIO_ResetBits(GPIOE, GPIO_Pin_10);
         GPIO_SetBits(GPIOE, GPIO_Pin_11);
-        delay_ms(DELAY_TIME);
+        vTaskDelay(500);
 
 		GPIO_ResetBits(GPIOE, GPIO_Pin_11);
         GPIO_SetBits(GPIOE, GPIO_Pin_12);
-        delay_ms(DELAY_TIME);
+        vTaskDelay(500);
 
 		GPIO_ResetBits(GPIOE, GPIO_Pin_12);
         GPIO_SetBits(GPIOE, GPIO_Pin_13);
-        delay_ms(DELAY_TIME);
+        vTaskDelay(500);
 
 		GPIO_ResetBits(GPIOE, GPIO_Pin_13);
         GPIO_SetBits(GPIOE, GPIO_Pin_14);
-        delay_ms(DELAY_TIME);
+        vTaskDelay(500);
 
 		GPIO_ResetBits(GPIOE, GPIO_Pin_14);
         GPIO_SetBits(GPIOE, GPIO_Pin_15);
-        delay_ms(DELAY_TIME);
+        vTaskDelay(500);
     }
+}
+
+int main (void)
+{
+    initAll();
+
+    xTaskCreate( prvTaskA, "TaskA", configMINIMAL_STACK_SIZE, NULL,
+        tskIDLE_PRIORITY, ( xTaskHandle * ) NULL );
+
+    vTaskStartScheduler();
+
+   //should never get here
+   for (;;);
 }
